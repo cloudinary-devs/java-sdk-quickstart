29eaf5f7d74c3d58f71ad994ce28bf2a6709925b
MeirFeinberg <meir.feinberg@cloudinary.com>
2022-10-24 15:10:30 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,15 +1,20 @@
 package org.example;
 
+// Import the required packages
+
 import com.cloudinary.*;
 import com.cloudinary.utils.ObjectUtils;
+import io.github.cdimascio.dotenv.Dotenv;
 
 import java.util.Map;
 
 
 public class Main {
     public static void main(String[] args) {
-        // replace `cloudinary://my_key:my_secret@my_cloud_name` with your environment variable
-        Cloudinary cloudinary = new Cloudinary("cloudinary://my_key:my_secret@my_cloud_name");
+        // Set your Cloudinary credentials
+
+        Dotenv dotenv = Dotenv.load();
+        Cloudinary cloudinary = new Cloudinary(dotenv.get("CLOUDINARY_URL"));
         cloudinary.config.secure = true;
         System.out.println(cloudinary.config.cloudName);
 
